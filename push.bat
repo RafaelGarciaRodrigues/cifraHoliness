256cc53f00817ae806c1208ba661b7b6ad409dae
RafaelGarciaRodrigues <rafael.garcia.rodrigues.com>
2026-09-04 09:13:09 -0300

    2026-09-04 09:13:09

--- push.bat
+++ push.bat
@@ -1,3 +1,4 @@
 @echo off
+call "%~dp0simplificado\inclui.bat"
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Location '%~dp0'; git add -A; $msg = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'; git commit -m $msg; git push"
 pause
